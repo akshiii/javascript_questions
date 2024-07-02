555e4a724630bd58cbc0c082b4855fb51ec910e3
Akshita Tyagi <aksitatyagi1001@gmail.com>
2024-07-02 11:00:20 +0530

    currying

--- debouncing.js
+++ debouncing.js
@@ -1,3 +1,8 @@
+/**
+ * Debouncing is a programming practise used to ensure that time consuming tasks do not fire so often that it stalls the performance of the web page.
+ * It says that we must make a function call only if the time-difference between 2 key press events is greater than 300(for example) milliseconds
+Debounce will send api calls only when user pauses while typing in input field for few seconds
+ */
 let counter = 0;
 const getData = () => {
   console.log("Fetching Data...", counter++);
