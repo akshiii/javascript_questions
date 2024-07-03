0d4c3883bcf605c696eaf66cd26aaef446955813
Akshita Tyagi <aksitatyagi1001@gmail.com>
2024-07-03 09:37:30 +0530

    flexbox

--- Closures/Basics.js
+++ Closures/Basics.js
@@ -1,5 +1,6 @@
 /**
-Combination of a function bundled togethar with references to its surrounding state. In other words, a closure gives you access to an outer function scope from inner function
+Combination of a function bundled togethar with references to its surrounding state. In other words, 
+a closure gives you access to an outer function scope from inner function
  */
 
 function makeFunc() {
