6764cd854a9efb51686a2004a14e061c7e6ea040
Akshita Tyagi <aksitatyagi1001@gmail.com>
2024-10-19 00:21:23 +0530

    Entity Relation in object

--- Interview_Questions/EntityRelation.js
+++ Interview_Questions/EntityRelation.js
@@ -1,37 +1,50 @@
-//Unsolved~~~
 let relationObj = {};
-function printRelationObject(list){
-	for([child , parent] of list){
-		console.log(child, parent);
+function fillObj([child, parent]) {
+  console.log(child, parent);
 
-		if(relationObj[child]){
-			let newChild = relationObj[child];
-			delete relationObj[child];
-			relationObj[parent] = {[child]: newChild};
-		}
-		else if(relationObj[parent]){
-			let innerObj = relationObj[parent];
-			innerObj[child] = {};
-		}
-		else{
-			printRelationObject()
-			relationObj[parent] = {[child] : {}};
-		}
-
-	};
+  if (relationObj[child]) {
+    let newChild = relationObj[child];
+    delete relationObj[child];
+    relationObj[parent] = { [child]: newChild };
+  } else if (relationObj[parent]) {
+    let innerObj = relationObj[parent];
+    innerObj[child] = {};
+  } else {
+    function findParent(newTree, parentToFind, childToAdd) {
+      if (Object.keys(relationObj).length != 0) {
+        for (const ch in newTree) {
+          if (ch == parentToFind) {
+            let newChild = newTree[ch];
+            newChild[childToAdd] = {};
+            return;
+          }
+          let innerObj = newTree[ch];
+          findParent(innerObj, parentToFind, childToAdd);
+        }
+      } else {
+        relationObj[parent] = { [child]: {} };
+      }
+    }
+    findParent(relationObj, parent, child);
+  }
 }
 
+function printRelationObject(list) {
+  for ([child, parent] of list) {
+    fillObj([child, parent]);
+  }
+}
 
 let input = [
-	["lion", "cat"],
-	["cheetah", "cat"],
-	["female_lion", "lion"],
-	["baby_lion", "lion"],
-	["cat", "mammal"],
-	["dog", "mammal"],
-	["mammal", "animal"],
-	["fish", "animal"],
-	["shark", "fish"],
-  ];
+  ["lion", "cat"],
+  ["cheetah", "cat"],
+  ["female_lion", "lion"],
+  ["baby_lion", "lion"],
+  ["cat", "mammal"],
+  ["dog", "mammal"],
+  ["mammal", "animal"],
+  ["fish", "animal"],
+  ["shark", "fish"],
+];
 printRelationObject(input);
 console.log(relationObj);
